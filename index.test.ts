91ebbfcddddcd4d3b3a87150d9d187a647195b86
dangoo <daniel.gooss@sinnerschrader.com>
2021-05-15 12:46:50 +0200

    chore: setup testing with jest

---
+++ index.test.ts
@@ -0,0 +1,5 @@
+describe('Test', () => {
+    it('should contain at least one test', () => {
+        expect(true).toBeDefined()
+    })
+})
---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: ['/node_modules/'],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: ['/node_modules/'],
+}
