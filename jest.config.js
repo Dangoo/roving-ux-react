222d24440041dcaf9cd7fdd73a52b827f9774210
dangoo <daniel.gooss@sinnerschrader.com>
2021-05-16 15:08:05 +0200

    chore: setup jest for react

--- jest.config.js
+++ jest.config.js
@@ -15,4 +15,6 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: ['/node_modules/'],
+
+  setupFilesAfterEnv: ['./jest.setup.ts'],
 }
